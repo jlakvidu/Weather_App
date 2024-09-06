ac26c3e7b6e4bfe35e6723e0f7d99f4f9fae414b
jlakvidu <jlakvidu@gmail.com>
2024-09-06 19:02:41 +0530

    third commit

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const apiKey = "fd091c84020448a584141337240609";
+    const timeZoneApiKey = "GPF627FUYAH0";
+    const searchButton = document.getElementById("searchButton");
+    const locationButton = document.getElementById("locationButton");
+    const darkModeToggle = document.getElementById("darkModeToggle");
+});
