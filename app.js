1c1219d1461bc3511d187f24bbe92de2092c52f4
jlakvidu <jlakvidu@gmail.com>
2024-09-07 18:34:46 +0530

    5th commit

--- app.js
+++ app.js
@@ -5,7 +5,20 @@ document.addEventListener("DOMContentLoaded", function () {
     const locationButton = document.getElementById("locationButton");
     const darkModeToggle = document.getElementById("darkModeToggle");
 
+    searchButton.addEventListener("click", () => {
+        const city = document.getElementById("search").value;   
+        if (city) fetchWeatherData(city);
+    });
+    
     darkModeToggle.addEventListener("change", () => {
         document.body.classList.toggle("dark-mode", darkModeToggle.checked);
     });
+
+    function updateTime(data) {
+        const date = new Date(data.formatted);
+        const options = { weekday: "long", day: "numeric", month: "short" };
+        document.getElementById("date").textContent = date.toLocaleDateString("en-US", options);
+        document.getElementById("time").textContent = date.toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit" });
+    }
+
 });
